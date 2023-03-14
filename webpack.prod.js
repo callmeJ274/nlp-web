3ce32546ba0f7dcaca9554f2d45289a439e99e31
call <chiptk472@gmail.com>
2023-03-14 19:16:19 +0700

    update

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,10 +3,19 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const WorkboxPlugin = require("workbox-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+	optimization: {
+		minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+	},
+	output: {
+		libraryTarget: "var",
+		library: "Client",
+	},
     module: {
         rules: [
             {
